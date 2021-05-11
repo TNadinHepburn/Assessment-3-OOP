d8aab4bc430a4b0618b58d48070831f41c721366
ThomasNadinHepburn <tom.nadin.hepburn@gmail.com>
2021-05-11 22:00:28 +0100

    added existing code to Card.cs

--- Assessment-3/Lincoln-Card-Game/Card.cs
+++ Assessment-3/Lincoln-Card-Game/Card.cs
@@ -6,5 +6,40 @@ namespace Lincoln_Card_Game
 {
     class Card
     {
+        public string Suit { get; }
+        public int Value { get; }
+
+        public Card(int value, string suit)
+        {
+            Suit = suit;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            //outputs the value and suit of a card e.g Value: 6 Suit: Heart
+            return $"{ValueToStr(Value)} ({Value}) of {Suit}'s";
+        }
+
+        private string ValueToStr(int value)
+        {
+            string[] strValues =
+            {
+                "Two",
+                "Three",
+                "Four",
+                "Five",
+                "Six",
+                "Seven",
+                "Eight",
+                "Nine",
+                "Ten",
+                "Jack",
+                "Queen",
+                "King",
+                "Ace"
+            };
+            return strValues[value - 2];
+        }
     }
 }
